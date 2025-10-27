9f6073e5229ced65eb26ca8cb2aa68564c795a6a
Mattt24 <matteoserra01@gmail.com>
2025-10-27 11:20:48 +0100

    primo commit

---
+++ progetto_social/src/test/java/it/social/restsocial/ProgettoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package it.social.restsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
